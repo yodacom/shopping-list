90d64f550488b92bc405e5d4b4ab7d6f27bfadc0
Jeremy Black <jb@yodacom.com>
2017-04-16 07:00:08 -0600

    added post functions and body-parser

--- server.js
+++ server.js
@@ -1,44 +1,43 @@
-var express = require('express');
-
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 
-
-app.post('/items', jsonParser, function(request, response) {
-    if (!('name' in request.body)) {
-        return response.sendStatus(400);
-    }
-
-    var item = storage.add(request.body.name);
-    response.status(201).json(item);
-});
 var Storage = {
-  add: function(name) {
-    var item = {name: name, id: this.setId};
-    this.items.push(item);
-    this.setId += 1;
-    return item;
-} 
+	add: function(name) {
+		var item = {name: name, id: this.setId};
+		this.items.push(item);
+		this.setId += 1;
+		return item;
+	} 
 };
 
 var createStorage = function() {
-  var storage = Object.create(Storage);
-  storage.items = [];
-  storage.setId = 1;
-  return storage;
-}
+	var storage = Object.create(Storage);
+	storage.items = [];
+	storage.setId = 1;
+	return storage;
+};
 
 var storage = createStorage();
 
-storage.add('Broad beans');
-storage.add('Tomatoes');
-storage.add('Peppers');
+storage.add("Broad beans");
+storage.add("Tomatoes");
+storage.add("Peppers");
 
 var app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/items', function(request, response) {
-    response.json(storage.items);
+app.get("/items", function(request, response) {
+	response.json(storage.items);
+});
+
+app.post("/items", jsonParser, function(request, response) {
+	if (!("name" in request.body)) {
+		return response.sendStatus(400);
+	}
+
+	var item = storage.add(request.body.name);
+	response.status(201).json(item);
 });
 
 app.listen(process.env.PORT || 8080, process.env.IP);
