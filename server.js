cb06817408179a36c48cba991d0ae9a9402636cb
Jeremy Black <jb@yodacom.com>
2017-04-17 16:23:14 -0600

    first attemp at getting edit/put to work

--- server.js
+++ server.js
@@ -10,10 +10,33 @@ var Storage = {
 		return item;
 	},
 
-	delete: function(name) {
+	delete: function(id) {
+		var index = this.items.findIndex((e)=>{
+			return e.id === +id;
+		});
+		console.log(index);
+
+		if (index == -1) {
+			return false;
+		} else {
+			this.items.splice(index, 1);
+			return true;
+		}
+		
+	},
+
+	edit: function(id){
+		var index= this.items.findIndex((e)=>{
+			return e.id === +id;
 		var item = {name: name, id: this.setId};
-		this.items.splice(id, 1);
-		return items;
+		});
+		console.log(index);
+		
+		if (index === -1) {
+			return false;
+		} else {
+			this.items.put[item] = name
+		}
 	}
 };
 
@@ -44,15 +67,25 @@ app.post("/items", jsonParser, function(request, response) {
 
 	var item = storage.add(request.body.name);
 	response.status(201).json(item);
-});
+}),
 
 app.delete("/items/:id", jsonParser, function(request, response) {
-	if (!("name" in request.body)) {
-		return response.sendStatus(400);
-	}
 
-	var item = storage.delete(request.body.name);
-	response.status(201).json(item);
-});
+	var success = storage.delete(request.params.id);
+	if (success)
+		response.status(200).json({messsage:"success"});
+	else {
+		response.status(400).json({messsage:"fail"});
+	}
+}),
+
+app.put("items/:id", jsonParser, function(request,response){
+	var success = storage.edit(request.params.id);
+	if (success) 
+		response.status(200).json({message:"success"});
+	else {
+		response.status(400).json({message:"fail"});
+	}
+}),
 
 app.listen(process.env.PORT || 8080, process.env.IP);
