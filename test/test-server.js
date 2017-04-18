644179a3469d2c1b9554c565aead77607a307f1e
Jeremy Black <jb@yodacom.com>
2017-04-18 10:35:00 -0600

    added mocha and stubbed iand heroku

---
+++ test/test-server.js
@@ -0,0 +1,17 @@
+describe('Shopping List', function () {
+  it("should list items on get");
+  it("should add an item on post");
+  it("should edit an item on put");
+  it("should delete an item on delete");
+  it("POST to an ID that exists");
+  it("POST without body data");
+  it("POST with something other than valid JSON");
+  it("PUT without an ID in the endpoint");
+  it("PUT with different ID in the endpoint than the body");
+  it("PUT to an ID that doesn't exist ");
+  it("PUT without body data");
+  it("PUT with something other than valid JSON");
+  it("DELETE an ID that doesn't exist");
+  it("DELETE without an ID in the endpoint");
+
+});
